9b4a74106e6b729c1fde5b1027f6879e39b1e613
searchme86 <searchme86@gmail.com>
2023-10-23 19:44:08 +0900

    ✨ add config of Custom Alias in

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import checker from 'vite-plugin-checker';
+import {defineConfig} from "vite";
+import checker from "vite-plugin-checker";
+import {resolve} from "path";
 
-export default {
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@script": resolve(__dirname, "src/scripts/"),
+      "@assets": resolve(__dirname, "src/assets/"),
+      "@images": resolve(__dirname, "src/assets/images/"),
+    },
+  },
   plugins: [
     checker({
       typescript: true,
@@ -9,4 +18,4 @@ export default {
       },
     }),
   ],
-};
+});
