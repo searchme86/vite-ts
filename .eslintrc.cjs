706311eaac38a623a89771012f56d992f904919b
searchme86 <searchme86@gmail.com>
2023-10-23 15:59:22 +0900

    ➕ ESLint import / prettier installed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,54 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+  },
+  settings: {
+    // 'import/resolver': {
+    //   node: {},
+    //   typescript: {
+    //     directory: './src',
+    //   },
+    // },
+    // 'import/parsers': { '@typescript-eslint/parser': '.ts' },
+  },
+  rules: {
+    // 'import/order': [
+    //   'error',
+    //   {
+    //     groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+    //     alphabetize: {
+    //       order: 'asc',
+    //       caseInsensitive: true,
+    //     },
+    //     'newlines-between': 'never',
+    //   },
+    // ],
+    // 'import/no-unresolved': 'off',
+    // 'import/export': 'off',
+  },
   root: true,
 };
